3abd5297dbaa89c150428b278bee9f4d6ac9709f
trainee <trainee@MININT-VLV5TMK.corp.capgemini.com>
2018-07-16 15:07:52 +0530

    Madhuri: My third commit.

--- MySampleGitProject/src/SampleClass.java
+++ MySampleGitProject/src/SampleClass.java
@@ -3,9 +3,9 @@ public class SampleClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("Make few changes !!!!");
-		
 		System.out.println("Hello World !!!!");
+		
+		System.out.println("Make few changes again !!!!");
 
 	}
 
